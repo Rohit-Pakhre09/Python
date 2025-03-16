aaf924af7dc8a70c110150a9802e49e4b4fe1d64
Rohit <pakhrerohit281@gmail.com>
2025-03-16 19:25:30 +0530

    Add files via upload

---
+++ Fundamental Questions/maximum-number.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the 1st number: "))
+number2 = int(input("Enter the 2nd number: "))
+number3 = int(input("Enter the 3rd number: "))
+
+result = max(number1, number2, number3)
+print(f"The maximum number from {number1}, {number2}, {number3} is {result}")
