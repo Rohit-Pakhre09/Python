db89bff22bffc6c3dcf029693bc281ac9d32709d
Rohit <pakhrerohit281@gmail.com>
2025-03-16 19:22:56 +0530

    Add files via upload

---
+++ Fundamental Questions/swapping.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Enter the 1st number : "))
+num_2 = int(input("Enter the 2nd number : "))
+print(f"Before Swapping Number 1: {num_1} & Number 2: {num_2}")
+
+num_1, num_2 = num_2, num_1
+print(f"After Swapping Number 1: {num_1} & Number 2: {num_2}")
