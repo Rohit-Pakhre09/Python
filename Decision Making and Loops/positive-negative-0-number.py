61b05230400249a4d179cd3aa6e17165e1d30dbc
Rohit <pakhrerohit281@gmail.com>
2025-03-17 16:26:28 +0530

    Add files via upload

---
+++ Decision Making and Loops/positive-negative-0-number.py
@@ -0,0 +1,8 @@
+num = int(input("Enter any number : "))
+
+if num > 0:
+    print(f"{num} is the Positive number.")
+elif num < 0:
+    print(f"{num} is the Negative number.")
+else:
+    print(f"{num} is the Neutral number.")
